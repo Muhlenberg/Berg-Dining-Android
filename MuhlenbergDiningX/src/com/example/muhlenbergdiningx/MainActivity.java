34eebe64ad3f97bd636162f9f33290d3a4dd76e5
Jalal Khan <jmankhan1@gmail.com>
2014-12-05 20:02:24 -0500

    weekday tab size has been increased. actionbar tab size seems to be unreasonably difficult to adjust, so i just spelled out the whole day and it seems to look ok

--- MuhlenbergDiningX/src/com/example/muhlenbergdiningx/MainActivity.java
+++ MuhlenbergDiningX/src/com/example/muhlenbergdiningx/MainActivity.java
@@ -75,7 +75,7 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
 	private DiningNavigationAdapter adapter;
 
 	private CharSequence title;
-	private String[] tabs = {"Su", "Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"};
+	private String[] tabs = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
 
 	public DiningXmlParser parser;
 	public MiscParser mParser;
